13f9be1073971822b01218ef74560e4c4ae6f5ef
Peco1503 <102172862+Peco1503@users.noreply.github.com>
2023-11-22 13:49:37 -0600

    Crete main.cpp
    hehe

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+    Actividad Integradoara 2
+
+        Franco Enrique Lugo Meza A00833951
+        Yuvan Maximiliano Solorzano Ortiz A00834129
+
+    Fecha de entrega: 12/11/2020
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hi bbyboi" << endl;
+
+    return 0;
+}
